890acb617cfef665190f5adb5eb23692baf0feb3
TonyMannion <anthony.mannion92@gmail.com>
2020-04-16 12:13:40 -0400

    Create gene_family_filter.py

---
+++ gene_family_filter.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import glob
+import os
+
+df_metadata = pd.read_csv('list_metadata.txt', sep='\t')
+lists = df_metadata['list_name'].dropna().tolist()
+
+for list in lists:
+	genome_list_include = df_metadata['include_' + str(list)].dropna().tolist()
+	genome_list_exclude = df_metadata['exclude_' + str(list)].dropna().tolist()
+	df = pd.read_csv('gene_family_groupby_out.txt', sep='\t') #output from gene_family_analysis.py
+	for genome in genome_list_include:
+		df=df[df[str(genome)]>0]
+	for genome in genome_list_exclude:
+		df= df[df[str(genome)]<=0]
+	df['category']=str(list)
+	df2 = df.iloc[:,[0,-1]].to_csv(str(list) + '_filter_out.txt', sep='\t',index=False)
+
+all_filenames = glob.glob('*_filter_out.txt')
+df_merged = pd.concat([pd.read_csv(f,sep='\t') for f in all_filenames])
+df_merged.to_csv('concat_filter.txt',  sep='\t', index=False)
+	#delete temp files
+for f in all_filenames:
+	os.remove(str(f))
+
+genome_list = df_metadata['genomes'].dropna().tolist()
+for genome in genome_list:
+	df_con = pd.read_csv('concat_filter.txt',  sep='\t')
+	df_an = pd.read_csv(str(genome)+'_median_gene_analysis.txt', sep='\t') #output from gene_family_analysis.py
+	df_merged = pd.merge(df_an, df_con, left_on='pgfam_id', right_on='pgfam', how="left").to_csv(str(genome)+'_annotations_filter.txt',  sep='\t', index=False)
