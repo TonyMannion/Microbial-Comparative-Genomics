e1eab3d37c016de03fbc22fa55cf0d0d567cf125
TonyMannion <anthony.mannion92@gmail.com>
2020-04-17 16:49:30 -0400

    Update make_clustermap.py

--- make_clustermap.py
+++ make_clustermap.py
@@ -8,23 +8,21 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-a', '--annotations', dest='annotations', default = 'no', help='Peform median gene count analysis on annotations files? Enter "yes" or "no". Requires annotations files obtained from "full_genome_analysis.py" script.')
-parser.add_argument('-p', '--PATRIC_features', dest='PATRIC_features', default = 'no', help='Peform median gene count analysis on PATRIC features? Enter "yes" or "no". Requires features file for genomes of interest downloaded from PATRIC from "downlaod_patric_features" argument above.')
+parser.add_argument('-m', '--metadata_file', dest='metadata_file', help='Specify metadata file. If using annotation metadata files, provide genome names of interest with under the header "genome_names". If downloading features from PATRIC, provide genome ids of interest under the header "genome_ids".')
+parser.add_argument('-a', '--annotations', dest='annotations', default = 'yes', help='Peform median gene count analysis on annotations files? Enter "yes" or "no". Requires annotations files obtained from "full_genome_analysis.py" script.')
 parser.add_argument('-d', '--downlaod_patric_features', dest='downlaod_patric_features', default = 'no', help='Download features from PATRIC? Enter "yes" or "no". Default is "yes". If features have already been downloaded, enter "no" to bypass this step.')
 parser.add_argument('-username', '--username', dest='username', help='If downloading from PATRIC, enter PATRIC login username.')
-parser.add_argument('-f', '--features_file', dest='features_file', default = 'features.txt', help='If features will be or have been downloaded PATRIC, specify the desired the features file name.')
-parser.add_argument('-m', '--metadata_file', dest='metadata_file', help='Specify metadata file. If using annotation metadata files, provide genome names of interest with under the header "genome_names". If downloading features from PATRIC, provide genome ids of interest under the header "genome_ids".')
 
 args = parser.parse_args()
 
 #gene analysis function
-def make_clustermap(input_file, genome_name, feature_type):
+def make_clustermap(input_file):
 	#input concatenated dataframe with gene families
-	df_concat = pd.read_csv(str(input_file),  sep='\t')
+	df_concat = pd.read_csv(str(input_file),sep='\t')
+	df_concat2 = df_concat.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'}) #clean up headers
 	#groupby
-	df_concat['count']=1
-	df_concat2 = df_concat.rename(columns={str(feature_type): 'pgfam'})
-	df_groupby = df_concat2.groupby([str(genome_name), 'pgfam'], as_index=False).sum().pivot(columns = str(genome_name), index = 'pgfam', values = 'count').fillna(0)
+	df_concat2['count']=1
+	df_groupby = df_concat2.groupby(['genome_name', 'pgfam'], as_index=False).sum().pivot(columns = 'genome_name', index = 'pgfam', values = 'count').fillna(0)
 	print 'Output gene family groupby dataframe...'
 	df_cm = df_groupby.transpose()
 	print "Generating clustermap..."
@@ -53,22 +51,23 @@ if str(args.downlaod_patric_features) == 'yes':
 	for genome_id, genome_name in zip(genome_ids_list,genome_name_list): 
 		print 'Downloading features for ' + str(genome_id)+ ' ' + str(genome_name) + ' from PATRIC...'
 		os.system('p3-echo -t genome_id ' + str(genome_id) + ' | p3-get-genome-features --in feature_type,CDS,rna --attr genome_name --attr sequence_id --attr patric_id --attr start --attr end --attr strand --attr product --attr pgfam_id --attr na_sequence --attr aa_sequence  > ' + str(genome_name) +'_annotation.txt')
-	df_concat = pd.concat([pd.read_csv(str(genome_name)+ '_annotation.txt',sep='\t') for genome_name in genome_name_list])
-	df_concat.to_csv('concatenated_annotations.txt',  sep='\t', index=False)
-
-#cluster map from PATRIC features
-if str(args.PATRIC_features) == 'yes':
-	#Execute gene analysis function
-	print "Performing gene analysis..."
-	make_clustermap('concatenated_annotations.txt', 'feature.genome_name', 'feature.pgfam_id')
+	df_concat = pd.concat([pd.read_csv(str(genome_name) + '_annotation.txt',sep='\t') for genome_name in genome_name_list])
+	df_concat2 = df_concat.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'}) #clean up headers
+	df_concat2.to_csv('concatenated_annotations.txt',  sep='\t', index=False)
 
 #cluster map from annotation metadata
 if str(args.annotations) == 'yes':
 	print "Performing gene analysis..." 
-	#concatenate
-	df_genome_names = pd.read_csv(str(args.metadata_file), sep='\t', usecols=['genome_name'])
-	genome_name_list = df_genome_names['genome_name'].dropna().tolist()
-	df_concat = pd.concat([pd.read_csv(str(genome_name)+ '_annotation.txt',sep='\t') for genome_name in genome_name_list])
-	df_concat.to_csv('concatenated_annotations.txt',  sep='\t', index=False)
-	#Execute gene analysis function
-	make_clustermap('concatenated_annotations.txt', 'genome_name', 'pgfam')
+	if os.path.isfile('concatenated_annotations.txt'):
+		print 'file already found'
+		make_clustermap('concatenated_annotations.txt')
+	else:
+		print 'making file'
+		df_genome_names = pd.read_csv(str(args.metadata_file), sep='\t', usecols=['genome_name'])
+		genome_name_list = df_genome_names['genome_name'].dropna().tolist()
+		df_concat = pd.concat([pd.read_csv(str(genome_name)+ '_annotation.txt',sep='\t') for genome_name in genome_name_list])
+		df_concat2 = df_concat.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'}) #clean up headers
+		df_concat2.to_csv('concatenated_annotations.txt',  sep='\t', index=False)
+		make_clustermap('concatenated_annotations.txt')
+
+os.remove('concatenated_annotations.txt')
