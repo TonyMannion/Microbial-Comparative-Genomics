e372b3e33ae7b13d6dd74d76444a35b50bb1764d
TonyMannion <anthony.mannion92@gmail.com>
2020-04-18 13:21:19 -0400

    Update pan-genome-tree.py

--- pan-genome-tree.py
+++ pan-genome-tree.py
@@ -1,38 +1,77 @@
+import os
+from os import path
+import argparse
+import sys
 import pandas as pd
 import numpy as np
-import os
-import argparse
 
-parser = argparse.ArgumentParser()
+parser=argparse.ArgumentParser()
 
-parser.add_argument('-username', '--username', dest='username', help='Enter PATRIC login username')
-parser.add_argument('-p', '--patric_features_dl', dest='patric_features_dl', default = 'yes', help='Download features from PATRIC? Enter "yes" or "no". Default is "yes". If features have already been downloaded, enter "no" to bypass this step.')
-parser.add_argument('-g', '--genome_id_file', dest='genome_ids', default = 'genome_ids.txt', help='Specify file containing PATRIC genome ids of interest. Header is "Genomes" and each genome id is on different row. Default is genome_ids.txt')
-parser.add_argument('-i', '--features_file', dest='features_file', default = 'features.txt', help='Input desired name for features file for genomes of interest downloaded from PATRIC.  Use a tab-delimited file')
-parser.add_argument('-a', '--index_column', dest='index_column', default = 'feature.genome_name', help='Specificy index column for genomes from feature file.  For example, use "genome_name" or "patric_id".  Default is "feature.genome_name".')
-parser.add_argument('-b', '--count_column', dest='count_column', default = 'feature.pgfam_id', help='Specificy count column for gene family from feature file.  Choose "plfam_id" for local protein family (genus-specific, called PLfam) or "pgfam_id" for global protein family (cross-genus, called PGfam).  Default is "feature.pgfam_id" for global protein family (cross-genus, called PGfam).')
-parser.add_argument('-o', '--output', dest='output', default = 'pan-genome-tree_out.txt', help='Specificy name for output file.  Default is pan-genome-tree_out.txt')
+parser.add_argument('-i', '--input_folder', dest='input_folder', help='Specify folder with annotations. If downloading annotations from PATRIC, this will create folder for files.')
+parser.add_argument('-o', '--output_folder', dest='output_folder', help='Specify output folder.')
+parser.add_argument('-m', '--metadata_file', dest='metadata_file', help='Specify metadata file. If using annotation metadata files, provide genome names of interest with under the header "genome_names". If downloading features from PATRIC, provide genome ids of interest under the header "genome_ids".')
+parser.add_argument('-d', '--downlaod_patric_features', dest='downlaod_patric_features', default = 'no', help='Download annotations from PATRIC? Enter "yes" or "no". Default is "yes". If annotations have already been downloaded, enter "no" to bypass this step.')
+parser.add_argument('-username', '--username', dest='username', help='If downloading from PATRIC, enter PATRIC login username.')
 
-args = parser.parse_args()
+args=parser.parse_args()
 
-if str(args.patric_features_dl) == 'yes':
+def make_tree(input_file):
+	print 'Creating binary matrix of protein families in PHYLIP format...'
+	#input concatenated dataframe with gene families
+	df_concat = pd.read_csv(output_folder +'/'+ str(input_file),sep='\t', usecols=['genome_name','pgfam'])
+	df_concat2 = df_concat.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'}) #clean up headers
+	#groupby
+	df_concat2['count']=1
+	df_groupby = df_concat2.replace(' ', '_', regex=True).groupby(['genome_name', 'pgfam'], as_index=False).sum().pivot(index = 'genome_name', columns = 'pgfam', values = 'count').fillna(0).astype(int)
+	for col in df_groupby:
+		#If value is greater than 0, replace with 1, otherwise replace with 0.
+		#Necessary because genome may have more than one count for protein family.
+		df_groupby[col] = np.where(df_groupby[col] > 0, 1, 0) 
+	total_rows=len(df_groupby.axes[0])
+	total_cols=len(df_groupby.axes[1])
+	df_groupby.to_csv(output_folder +'/'+'pan-genome-tree_out.txt', sep='\t') 
+	with open(output_folder +'/'+'pan-genome-tree_out.txt', "rb") as f:
+		lines = f.readlines()
+	lines[0] = str(total_rows) + " " + str(total_cols) + "\n"
+	with open(output_folder +'/'+'pan-genome-tree_out.txt', "w") as f:
+		f.writelines(lines)
+
+#make output folder
+output_folder = 'pangenome_tree_' + str(args.output_folder)
+os.mkdir(output_folder)
+
+#download features from PATRIC
+if str(args.downlaod_patric_features) == 'yes':
 	print 'Logging into PATRIC...'
 	os.system('p3-login ' + str(args.username))
-	print 'Downloading features from PATRIC...'
-	os.system('p3-get-genome-features --input ' + str(args.genome_ids) + ' --attr genome_name --attr patric_id --attr gene_id --attr plfam_id --attr pgfam_id --attr product > ' + str(args.features_file))
-
-print 'Creating binary matrix of protein families in PHYLIP format...'
-
-df1 = pd.read_csv(str(args.features_file), sep='\t', usecols=[str(args.index_column),str(args.count_column)])
-df1['count']=1
-df2 = df1.replace(' ', '_', regex=True).groupby([str(args.index_column), str(args.count_column)], as_index=False).sum().pivot(index = str(args.index_column), columns = str(args.count_column), values = 'count').fillna(0).astype(int)
-for col in df2:
-	df2[col] = np.where(df2[col] > 0, 1, 0) # If value is greater than 0, replace with 1, otherwise replace with 0.  Necessary because genome may have more than one count for protein family.
-total_rows=len(df2.axes[0])
-total_cols=len(df2.axes[1])
-df2.to_csv(str(args.output), sep='\t') 
-with open(str(args.output), "rb") as f:
-	lines = f.readlines()
-lines[0] = str(total_rows) + " " + str(total_cols) + "\n"
-with open(str(args.output), "w") as f:
-	f.writelines(lines)
+	df_genome_names = pd.read_csv(str(args.metadata_file), sep='\t', usecols=['genome_ids','genome_name'])
+	genome_ids_list = df_genome_names['genome_ids'].dropna().tolist()
+	genome_name_list = df_genome_names['genome_name'].dropna().tolist()
+	zip(genome_ids_list,genome_name_list)
+	for genome_id, genome_name in zip(genome_ids_list,genome_name_list): 
+		print 'Downloading features for ' + str(genome_id)+ ' ' + str(genome_name) + ' from PATRIC...'
+		#make input folder if not already present
+		if not os.path.exists(str(args.input_folder)):
+			os.mkdir(str(args.input_folder))
+		os.system('p3-echo -t genome_id ' + str(genome_id) + ' | p3-get-genome-features --in feature_type,CDS,rna --attr genome_name --attr sequence_id --attr patric_id --attr start --attr end --attr strand --attr product --attr pgfam_id --attr na_sequence --attr aa_sequence  > ' + str(args.input_folder) +'/'+str(genome_name) +'_annotation.txt')
+		df_genome = pd.read_csv(str(args.input_folder) +'/'+str(genome_name) +'_annotation.txt', sep='\t')
+		df_genome2=df_genome.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'})
+		df_genome2.to_csv(str(args.input_folder) +'/'+str(genome_name) +'_annotation.txt', sep='\t', index=False)
+	df_concat = pd.concat([pd.read_csv(str(args.input_folder) +'/'+str(genome_name) + '_annotation.txt',sep='\t') for genome_name in genome_name_list])
+	df_concat2 = df_concat.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'}) #clean up headers
+	df_concat2.to_csv(output_folder +'/'+'concatenated_annotations.txt',  sep='\t', index=False)
+#make tree
+print "Performing gene analysis..." 
+if os.path.isfile(output_folder +'/'+ 'concatenated_annotations.txt'):
+	print 'file already found'
+	make_tree('concatenated_annotations.txt')
+else:
+	print 'making file'
+	df_genome_names = pd.read_csv(str(args.metadata_file), sep='\t', usecols=['genome_name'])
+	genome_name_list = df_genome_names['genome_name'].dropna().tolist()
+	df_concat = pd.concat([pd.read_csv(str(args.input_folder) +'/'+ str(genome_name)+ '_annotation.txt',sep='\t') for genome_name in genome_name_list])
+	df_concat2 = df_concat.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'}) #clean up headers
+	df_concat2.to_csv(output_folder +'/'+ 'concatenated_annotations.txt',  sep='\t', index=False)
+	make_tree('concatenated_annotations.txt')
+#remove intermediate files
+os.remove(output_folder +'/'+'concatenated_annotations.txt')
