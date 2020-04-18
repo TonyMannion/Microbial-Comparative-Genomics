9a0a5c9ba7f3e1d3ab2dfde1c07892650d43e284
TonyMannion <anthony.mannion92@gmail.com>
2020-04-17 21:03:45 -0400

    Create core_unqiue_gene_analysis.py

---
+++ core_unqiue_gene_analysis.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+import sys 
+import os
+import argparse
+import glob
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument('-m', '--metadata_file', dest='metadata_file', help='Specify metadata file. If using annotation metadata files, provide genome names of interest with under the header "genome_names". If downloading features from PATRIC, provide genome ids of interest under the header "genome_ids".')
+parser.add_argument('-a', '--annotations', dest='annotations', default = 'yes', help='Peform median gene count analysis on annotations files? Enter "yes" or "no". Requires annotations files obtained from "full_genome_analysis.py" script.')
+parser.add_argument('-d', '--downlaod_patric_features', dest='downlaod_patric_features', default = 'no', help='Download features from PATRIC? Enter "yes" or "no". Default is "yes". If features have already been downloaded, enter "no" to bypass this step.')
+parser.add_argument('-username', '--username', dest='username', help='If downloading from PATRIC, enter PATRIC login username.')
+
+args=parser.parse_args()
+
+#core unique genes filter function
+def core_unique_genes(input_file):
+	#input concatenated dataframe with gene families
+	df_concat = pd.read_csv(str(input_file),sep='\t')
+	df_concat2 = df_concat.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'}) #clean up headers
+	#groupby
+	df_concat2['count']=1
+	df_groupby = df_concat2.groupby(['genome_name', 'pgfam'], as_index=False).sum().pivot(columns = 'genome_name', index = 'pgfam', values = 'count').fillna(0).to_csv('gene_family_groupby_out.txt', sep='\t') 
+	print 'Output gene family groupby dataframe...'
+	df_genome_names = pd.read_csv(str(args.metadata_file), sep='\t', usecols=['genome_name'])
+	genome_name_list=df_genome_names['genome_name'].dropna().tolist()	
+	#core
+	df=pd.read_csv('gene_family_groupby_out.txt', sep='\t')
+	for col in df.columns[1:]:
+		df=df[df[str(col)]>0]
+	df['core_unique_gene']='core'
+	df2 = df.iloc[:,[0,-1]].to_csv('temp_core_filter_out.txt', sep='\t',index=False)
+
+	for genome in genome_name_list:
+		exclude_genome_list = df_genome_names['genome_name'].dropna().tolist()
+		exclude_genome_list.remove(str(genome))
+		df = pd.read_csv('gene_family_groupby_out.txt', sep='\t')
+		df=df[df[str(genome)]>0]
+		for excluded_genome in exclude_genome_list:
+			df= df[df[str(excluded_genome)]<=0]
+		df['core_unique_gene']='unique_'+str(genome)
+		df2 = df.iloc[:,[0,-1]].to_csv('temp_'+str(genome) + '_unique_filter_out.txt', sep='\t',index=False)
+	#concat
+	temp_filter_files = glob.glob('temp_' + '*_filter_out.txt')
+	df_concat = pd.concat([pd.read_csv(temp_file,sep='\t') for temp_file in temp_filter_files])
+	#delete temp files
+	for temp_file in temp_filter_files:
+		os.remove(str(temp_file))
+	#merge
+	for genome in genome_name_list:
+		df_an = pd.read_csv(str(genome)+'_annotation.txt', sep='\t')
+		df_merged = pd.merge(df_an, df_concat, left_on='pgfam', right_on='pgfam', how="left").to_csv(str(genome)+'_annotation.txt',  sep='\t', index=False)
+		print 'Done with '+ str(genome)
+
+#download features from PATRIC
+if str(args.downlaod_patric_features) == 'yes':
+	print 'Logging into PATRIC...'
+	os.system('p3-login ' + str(args.username))
+	df_genome_names = pd.read_csv(str(args.metadata_file), sep='\t', usecols=['genome_ids','genome_name'])
+	genome_ids_list = df_genome_names['genome_ids'].dropna().tolist()
+	genome_name_list = df_genome_names['genome_name'].dropna().tolist()
+	zip(genome_ids_list,genome_name_list)
+	for genome_id, genome_name in zip(genome_ids_list,genome_name_list): 
+		print 'Downloading features for ' + str(genome_id)+ ' ' + str(genome_name) + ' from PATRIC...'
+		os.system('p3-echo -t genome_id ' + str(genome_id) + ' | p3-get-genome-features --in feature_type,CDS,rna --attr genome_name --attr sequence_id --attr patric_id --attr start --attr end --attr strand --attr product --attr pgfam_id --attr na_sequence --attr aa_sequence  > ' + str(genome_name) +'_annotation.txt')
+		df_genome = pd.read_csv(str(genome_name) +'_annotation.txt', sep='\t')
+		df_genome2=df_genome.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'})
+		df_genome2.to_csv(str(genome_name) +'_annotation.txt', sep='\t', index=False)
+	df_concat = pd.concat([pd.read_csv(str(genome_name) + '_annotation.txt',sep='\t') for genome_name in genome_name_list])
+	df_concat2 = df_concat.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'}) #clean up headers
+	df_concat2.to_csv('concatenated_annotations.txt',  sep='\t', index=False)
+
+#cluster map from annotation metadata
+if str(args.annotations) == 'yes':
+	print "Performing gene analysis..." 
+	if os.path.isfile('concatenated_annotations.txt'):
+		print 'file already found'
+		core_unique_genes('concatenated_annotations.txt')
+	else:
+		print 'making file'
+		df_genome_names = pd.read_csv(str(args.metadata_file), sep='\t', usecols=['genome_name'])
+		genome_name_list = df_genome_names['genome_name'].dropna().tolist()
+		df_concat = pd.concat([pd.read_csv(str(genome_name)+ '_annotation.txt',sep='\t') for genome_name in genome_name_list])
+		df_concat2 = df_concat.rename(columns={'feature.genome_name': 'genome_name','feature.pgfam_id': 'pgfam'}) #clean up headers
+		df_concat2.to_csv('concatenated_annotations.txt',  sep='\t', index=False)
+		core_unique_genes('concatenated_annotations.txt')
+
+os.remove('concatenated_annotations.txt')
+os.remove('gene_family_groupby_out.txt')
